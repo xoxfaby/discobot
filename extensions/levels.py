62b122de767e46051049944eace9b59025ef0155
Jared Shields <jwshields2006@hotmail.com>
2018-01-16 16:55:10 -0800

    Smol update to leveling

--- extensions/levels.py
+++ extensions/levels.py
@@ -7,3 +7,36 @@ class LevelSystem:
     def __init__(self, bot):
         self.bot = bot
         print(f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}: Addon "{self.__class__.__name__}" loaded')
+
+    async def levelgoals(self):
+        goaldict = dict()
+        goaldict["images"] = int(50)
+        goaldict['messages'] = int(100)
+        goaldict['onlinetime'] = int(48)
+
+    async def rewards(self):
+        rewarddict = dict()
+        rewarddict['imagemessage'] = int(2)
+        rewarddict['textmessage'] = int(1)
+        rewarddict['onlinetimeincrement'] = int(1)
+
+    async def levelup(self):
+        pass
+
+    async def randomchooser(self):
+        pass
+
+    async def levelstodb(self):
+        pass
+
+    async def on_message(self, ctx):
+        pass
+
+    @commands.command()
+    @commands.check(dbotchecks.check_server_admin_or_botowner)
+    async def levelgoals(self, ctx, user):
+        pass
+
+
+def setup(dbot):
+    dbot.add_cog(LevelSystem(dbot))
